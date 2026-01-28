ebbcdd5e7db528f7662be0d9936380cad57df322
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-01-28 10:53:35 -0500

    fix(infra): Correct S3 object KMS key variable name typo
    - Fix typo in kms_key_id variable reference from `kms_key_aliasI` to `kms_key_alias`
    - Ensure proper variable resolution for S3 object encryption configuration
    - Resolves potential runtime error from undefined variable reference

--- infra/aws/tf/modules/s3/main.tf
+++ infra/aws/tf/modules/s3/main.tf
@@ -45,5 +45,5 @@ resource "aws_s3_object" "folders" {
   content_type = "application/x-directory"
 
   server_side_encryption = "aws:kms"
-  kms_key_id             = var.s3_bucket.kms_key_aliasI 
+  kms_key_id             = var.s3_bucket.kms_key_alias
 }
