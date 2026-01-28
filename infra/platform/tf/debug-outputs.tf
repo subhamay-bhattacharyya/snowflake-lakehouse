a7744c15630384c8c88307467079bfd6aae7e332
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-01-27 20:51:13 -0500

    chore(infra): Remove commented Snowflake debug outputs
    - Remove Snowflake AWS IAM user ARNs debug output
    - Remove Snowflake external ID debug output
    - Remove local storage integrations debug output
    - Clean up debug outputs file as Phase 2 Snowflake deployment remains pending

--- infra/platform/tf/debug-outputs.tf
+++ infra/platform/tf/debug-outputs.tf
@@ -17,21 +17,3 @@
 #   description = "IAM Role configuration"
 #   value       = local.iam_role_config
 # }
-
-# ----------------------------------------------------------------------------
-# Snowflake Outputs (COMMENTED OUT - Phase 2 not deployed)
-# ----------------------------------------------------------------------------
-# output "snowflake_aws_iam_user_arns" {
-#   description = "Snowflake IAM user ARNs - use in AWS IAM trust policy"
-#   value       = module.snowflake.storage_integrations.bronze_s3.storage_aws_iam_user_arn
-# }
-
-# output "snowflake_external_id" {
-#   description = "External ID for Snowflake IAM trust policy"
-#   value       = module.snowflake.storage_integrations.bronze_s3.storage_aws_external_id
-#   sensitive   = false
-# }
-# output "local_storage_integrations" {
-#   description = "Storage integrations"
-#   value       = local.storage_integrations
-# }
